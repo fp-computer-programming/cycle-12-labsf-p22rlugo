65131bba7bbb4cd2085a9760857d740769e11061
p22rlugo <p22rlugo@fairfieldprep.org>
2022-02-16 09:51:55 -0500

    Add files

---
+++ lab_12-3.py
@@ -0,0 +1,7 @@
+# Ryan Lugo: RJL 2/16/22
+
+weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday")
+weekends = ("Saturday","Sunday")
+
+days_of_the_week = weekdays + weekends
+print(days_of_the_week)
