0525d1a5375eb6a90faa82fcc33a5fb9efc75923
p22rlugo <p22rlugo@fairfieldprep.org>
2022-02-23 09:56:33 -0500

    Add files

--- lab_12-4.py
+++
@@ -1,8 +0,0 @@
-# Ryan Lugo: RJL 2/16/22
-
-day_date = [2,3,4,5,6]
-
-tup = ("Mon","Tues","Wednesday","Thursday","Friday","Saturday","Sunday")
-
-for i,v in enumerate(day_date):
-    print(tup[v])
--- lab_12-5.py
+++
@@ -1,7 +0,0 @@
-# Ryan Lugo: RJL 2/17/22
-
-row = {"B202-15":"Sean","B202-14":"Luis","B202-13":"Ryan","B202-12":"James","B202-11":"Jack"}
-
-print(row)
-
-row["B202-13"] = "Bob"
